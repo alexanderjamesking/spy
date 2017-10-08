db17c73f6273e485483f77fc50a51b6bad10879d
Alex King <alex@alexanderjamesking.com>
2017-10-08 21:29:00 +0100

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-spy "0.3.0"
+(defproject clj-spy "0.4.0-SNAPSHOT"
   :description "Spy - a Clojure and ClojureScript library for stubs, spies and mocks"
   :url "http://www.alexanderjamesking.com"
   :license {:name "MIT License"
