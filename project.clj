c98282baaed02cd184e56ec0d0bc73b73ec8ab5a
Alex King <alex@alexanderjamesking.com>
2018-05-23 07:40:28 +0100

    more js tests

--- project.clj
+++ project.clj
@@ -15,9 +15,30 @@
                                   [org.clojure/tools.namespace "0.2.11"]]}}
   :deploy-repositories [["releases" :clojars]]
   :clean-targets ["target" "out"]
-  :cljsbuild {:builds [{:id "test"
+  :cljsbuild {:builds [{:id "test-phantom"
                         :source-paths ["src" "test"]
-                        :compiler {:output-to "target/test.js"
+                        :compiler {:output-to "target/test-phantom/test.js"
+                                   :output-dir "target/test-phantom/out"
                                    :main 'spy.runner
-                                   :optimizations :whitespace}}]}
-  :aliases {"test" ["do" ["clean"] ["test"] ["doo" "nashorn" "test" "once"]]})
+                                   :optimizations :none
+                                   :process-shim false}}
+                       {:id "test-nashorn"
+                        :source-paths ["src" "test"]
+                        :compiler {:output-to "target/test-nashorn/test.js"
+                                   :output-dir "target/test-nashorn/out"
+                                   :main 'spy.runner
+                                   :optimizations :simple
+                                   :process-shim false}}
+                       {:id "test-node"
+                        :source-paths ["src" "test"]
+                        :compiler {:target :nodejs
+                                   :output-to "target/test-node/test.js"
+                                   :output-dir "target/test-node/out"
+                                   :main 'spy.runner
+                                   :optimizations :none
+                                   :process-shim false}}]}
+  :aliases {"test-phantom" ["doo" "phantom" "test-phantom" "once"]
+            "test-nashorn" ["doo" "nashorn" "test-nashorn" "once"]
+            "test-node"    ["doo" "node" "test-node" "once"]
+            "test-cljs"    ["do" ["test-phantom"] ["test-nashorn"] ["test-node"]]
+            "test-all"     ["do" ["clean"] ["test"] ["test-cljs"]]})
--- src/spy/assert.clj
+++ src/spy/assert.clj
@@ -1,60 +1,66 @@
 (ns spy.assert
-  (:require [clojure.test :refer [is]]
-            [spy.core :as spy]))
+  (:require [spy.core :as spy]))
 
-(defn- calls [n]
+(def ^:private ^:no-doc cljs? (boolean (find-ns 'cljs.analyzer)))
+
+(defmacro quick-test []
+  (if cljs?
+    `(cljs.test/is (= 2 2))
+    `(clojure.test/is (= 3 3))))
+
+#_(defn- calls [n]
   (if (= 1 n)
     "call"
     "calls"))
 
-(defn calls-message [f expected-count]
+#_(defn calls-message [f expected-count]
   (str "Expected " expected-count " " (calls expected-count) ", "
        "received " (spy/call-count f) " " (calls (spy/call-count f)) "."))
 
-(defn called-n-times?
+#_(defn called-n-times?
   [f n]
   (is (spy/called-n-times? f n)
       (calls-message f n)))
 
-(defn not-called?
+#_(defn not-called?
   [f]
   (is (spy/not-called? f)
       (calls-message f 0)))
 
-(defn called-once?
+#_(defn called-once?
   [f]
   (is (spy/called-once? f)
       (calls-message f 1)))
-
+#_
 (defn called-with?
   [f & args]
   (is (apply spy/called-with? f args)
       (str "Spy was not called with " args ".\n\nCalls:\n" (spy/calls f))))
-
+#_
 (defn not-called-with?
   [f & args]
   (is (apply spy/not-called-with? f args)
       (str "Spy was called with " args ".\n\nCalls:\n" (spy/calls f))))
-
+#_
 (defn called-once-with?
   [f & args]
   (is (apply spy/called-once-with? f args)
       (str "Spy was called once with " args)))
-
+#_
 (defn called-at-least-n-times?
   [f n]
   (is (spy/called-at-least-n-times? f n)
       (str "Expected at least " n " " (calls n) ", "
            "received " (spy/call-count f) " " (calls (spy/call-count f)) ".")))
-
+#_
 (defn called?
   [f])
-
+#_
 (defn called-at-least-once?
   [f])
-
+#_
 (defn called-no-more-than-n-times?
   [f n])
-
+#_
 (defn called-no-more-than-once?
   [f])
