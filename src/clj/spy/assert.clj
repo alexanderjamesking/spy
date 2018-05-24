e32fef032323d24456e13c5f9ba48c0153de3740
Alex King <alex@alexanderjamesking.com>
2018-05-24 10:46:24 +0100

    changed messages

--- src/clj/spy/assert.clj
+++ src/clj/spy/assert.clj
@@ -2,16 +2,21 @@
   (:require [spy.core :as spy]
             [clojure.test :refer [is]]))
 
-(defn calls [n]
+(defn calls
+  "Returns call if n is one, otherwise returns calls."
+  [n]
   (if (= 1 n)
     "call"
     "calls"))
 
-(defn calls-message [f expected-count]
+(defn calls-message
+  "Returns message with expected and actual call counts.'"
+  [f expected-count]
   (str "Expected " expected-count " " (calls expected-count) ", "
-       "received " (spy/call-count f) " " (calls (spy/call-count f)) "."))
+       "actual: " (spy/call-count f) " " (calls (spy/call-count f)) "."))
 
 (defmacro called-n-times?
+  "Throws "
   [f n]
   `(is (spy/called-n-times? ~f ~n)
        (calls-message ~f ~n)))
@@ -29,23 +34,23 @@
 (defmacro called-with?
   [f & args]
   `(is (spy/called-with? ~f ~@args)
-       (str "Expected a call with " '~args ".\n\nCalls:\n" (spy/calls ~f))))
+       (str "Expected a call with " '~args ".\nActual: " (spy/calls ~f))))
 
 (defmacro not-called-with?
   [f & args]
   `(is (spy/not-called-with? ~f ~@args)
-       (str "Expected no calls with " '~args ".\n\nCalls:\n" (spy/calls ~f))))
+       (str "Expected no calls with " '~args ".\nActual: " (spy/calls ~f))))
 
 (defmacro called-once-with?
   [f & args]
   `(is (spy/called-once-with? ~f ~@args)
-      (str "Expected one call with " '~args ".\n\nCalls:\n" (spy/calls ~f))))
+      (str "Expected one call with " '~args ".\nActual: " (spy/calls ~f))))
 
 (defmacro called-at-least-n-times?
   [f n]
   `(is (spy/called-at-least-n-times? ~f ~n)
        (str "Expected at least " ~n " " (calls ~n) ", "
-            "received " (spy/call-count ~f) " " (calls (spy/call-count ~f)) ".")))
+            "actual: " (spy/call-count ~f) " " (calls (spy/call-count ~f)) ".")))
 
 (defmacro called?
   [f]
@@ -59,7 +64,7 @@
   [f n]
   `(is (spy/called-no-more-than-n-times? ~f ~n)
        (str "Expected no more than " ~n " " (calls ~n) ", "
-            "received " (spy/call-count ~f) " " (calls (spy/call-count ~f)) ".")))
+            "actual: " (spy/call-count ~f) " " (calls (spy/call-count ~f)) ".")))
 
 (defmacro called-no-more-than-once?
   [f]
--- test/clj/spy/assert_test.clj
+++ test/clj/spy/assert_test.clj
@@ -16,30 +16,30 @@
   (let [f (spy/spy)]
     (f)
     (assert-failure #(assert/not-called? f)
-                    "Expected 0 calls, received 1 call.")))
+                    "Expected 0 calls, actual: 1 call.")))
 
 (deftest called-once-test
   (let [f (spy/spy)]
     (f)
     (f)
     (assert-failure #(assert/called-once? f)
-                    "Expected 1 call, received 2 calls.")))
+                    "Expected 1 call, actual: 2 calls.")))
 
 (deftest called-n-times-test
   (assert-failure #(assert/called-n-times? (spy/spy) 5)
-                  "Expected 5 calls, received 0 calls."))
+                  "Expected 5 calls, actual: 0 calls."))
 
 (deftest called-with-test
   (let [f (spy/spy +)]
     (f 1 2)
     (assert-failure #(assert/called-with? f 1 2 3)
-                    "Expected a call with (1 2 3).\n\nCalls:\n[(1 2)]")))
+                    "Expected a call with (1 2 3).\nActual: [(1 2)]")))
 
 (deftest not-called-with-test
   (let [f (spy/spy +)]
     (f 1 2)
     (assert-failure #(assert/not-called-with? f 1 2)
-                    "Expected no calls with (1 2).\n\nCalls:\n[(1 2)]")))
+                    "Expected no calls with (1 2).\nActual: [(1 2)]")))
 
 (deftest called-once-with-test
   (let [f (spy/spy str)]
@@ -48,16 +48,16 @@
 
     (f "foo bar")
     (assert-failure #(assert/called-once-with? f "foo bar")
-                    "Expected one call with (\"foo bar\").\n\nCalls:\n[(\"hello world!\") (\"foo bar\")]")))
+                    "Expected one call with (\"foo bar\").\nActual: [(\"hello world!\") (\"foo bar\")]")))
 
 (deftest called-at-least-n-times-test
   (let [f (spy/stub 42)]
     (assert-failure #(assert/called-at-least-n-times? f 1)
-                    "Expected at least 1 call, received 0 calls.")
+                    "Expected at least 1 call, actual: 0 calls.")
     (f)
     (is (assert/called-at-least-n-times? f 1))
     (assert-failure #(assert/called-at-least-n-times? f 33)
-                    "Expected at least 33 calls, received 1 call.")
+                    "Expected at least 33 calls, actual: 1 call.")
 
     (doall (repeatedly 42 f))
 
@@ -66,14 +66,14 @@
 (deftest called-test
   (let [f (spy/stub 42)]
     (assert-failure #(assert/called? f)
-                    "Expected at least 1 call, received 0 calls.")
+                    "Expected at least 1 call, actual: 0 calls.")
     (f)
     (is (assert/called? f))))
 
 (deftest called-at-least-once-test
   (let [f (spy/stub 42)]
     (assert-failure #(assert/called-at-least-once? f)
-                    "Expected at least 1 call, received 0 calls.")
+                    "Expected at least 1 call, actual: 0 calls.")
     (f)
     (is (assert/called-at-least-once? f))))
 
@@ -84,7 +84,7 @@
     (f)
     (f)
     (assert-failure #(assert/called-no-more-than-n-times? f 2)
-                    "Expected no more than 2 calls, received 3 calls.")))
+                    "Expected no more than 2 calls, actual: 3 calls.")))
 
 (deftest called-no-more-than-once-test
   (let [f (spy/spy)]
@@ -93,4 +93,4 @@
     (is (assert/called-no-more-than-once? f))
     (f)
     (assert-failure #(assert/called-no-more-than-once? f)
-                    "Expected no more than 1 call, received 2 calls.")))
+                    "Expected no more than 1 call, actual: 2 calls.")))
