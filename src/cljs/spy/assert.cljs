8305859d4534bdaf1536c11e66cd47d0cca94fe7
Alex King <alex@alexanderjamesking.com>
2018-05-24 12:25:07 +0100

    docs

--- src/cljs/spy/assert.cljs
+++ src/cljs/spy/assert.cljs
@@ -1,4 +1,12 @@
-(ns spy.assert
+(ns ^{:author "Alexander James King",
+      :doc "Assertion functions wrapping spy.core functions,
+      providing assertions using cljs.test/is and messages
+      to aid debugging when test expectations are not met.
+
+      Differs from the Clojure version as macros are not used
+      due to added complexity and the fact that cljs does not
+      provide the line number on failure at present."}
+    spy.assert
   (:require [spy.core :as spy]
             [spy.assert-messages :refer [calls calls-message]]
             [cljs.test :refer [do-report] :refer-macros [is]]))
