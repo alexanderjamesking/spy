5d37cdff492e8d7811cf43839e0e32ae94319506
Alex King <alex@alexanderjamesking.com>
2018-05-24 10:57:43 +0100

    refactor test to use macros so failures show in the correct place in emacs

--- test/clj/spy/assert_test.clj
+++ test/clj/spy/assert_test.clj
@@ -3,42 +3,41 @@
             [clojure.test :refer [deftest testing is testing-contexts-str report]]
             [spy.assert :as assert]))
 
-(defn- assert-failure [assert-fn expected-message]
-  (let [reporter (spy/spy)]
-    (binding [report reporter]
-      (assert-fn))
-
-    (let [report (first (spy/first-call reporter))]
-      (is (= :fail (:type report)) (str "Expected assertion" (testing-contexts-str) " to fail"))
-      (is (= expected-message (:message report))))))
+(defmacro assert-failure [assert-fn expected-message]
+  `(let [reporter# (spy/spy)]
+     (binding [report reporter#]
+       ~assert-fn)
+     (let [report# (first (spy/first-call reporter#))]
+       (is (= :fail (:type report#)) (str "Expected assertion" (testing-contexts-str) " to fail"))
+       (is (= ~expected-message (:message report#))))))
 
 (deftest not-called-test
   (let [f (spy/spy)]
     (f)
-    (assert-failure #(assert/not-called? f)
+    (assert-failure (assert/not-called? f)
                     "Expected 0 calls, actual: 1 call.")))
 
 (deftest called-once-test
   (let [f (spy/spy)]
     (f)
     (f)
-    (assert-failure #(assert/called-once? f)
+    (assert-failure (assert/called-once? f)
                     "Expected 1 call, actual: 2 calls.")))
 
 (deftest called-n-times-test
-  (assert-failure #(assert/called-n-times? (spy/spy) 5)
+  (assert-failure (assert/called-n-times? (spy/spy) 5)
                   "Expected 5 calls, actual: 0 calls."))
 
 (deftest called-with-test
   (let [f (spy/spy +)]
     (f 1 2)
-    (assert-failure #(assert/called-with? f 1 2 3)
+    (assert-failure (assert/called-with? f 1 2 3)
                     "Expected a call with (1 2 3).\nActual: [(1 2)]")))
 
 (deftest not-called-with-test
   (let [f (spy/spy +)]
     (f 1 2)
-    (assert-failure #(assert/not-called-with? f 1 2)
+    (assert-failure (assert/not-called-with? f 1 2)
                     "Expected no calls with (1 2).\nActual: [(1 2)]")))
 
 (deftest called-once-with-test
@@ -47,16 +46,16 @@
     (is (assert/called-once-with? f "hello world!"))
 
     (f "foo bar")
-    (assert-failure #(assert/called-once-with? f "foo bar")
+    (assert-failure (assert/called-once-with? f "foo bar")
                     "Expected one call with (\"foo bar\").\nActual: [(\"hello world!\") (\"foo bar\")]")))
 
 (deftest called-at-least-n-times-test
   (let [f (spy/stub 42)]
-    (assert-failure #(assert/called-at-least-n-times? f 1)
+    (assert-failure (assert/called-at-least-n-times? f 1)
                     "Expected at least 1 call, actual: 0 calls.")
     (f)
     (is (assert/called-at-least-n-times? f 1))
-    (assert-failure #(assert/called-at-least-n-times? f 33)
+    (assert-failure (assert/called-at-least-n-times? f 33)
                     "Expected at least 33 calls, actual: 1 call.")
 
     (doall (repeatedly 42 f))
@@ -65,14 +64,14 @@
 
 (deftest called-test
   (let [f (spy/stub 42)]
-    (assert-failure #(assert/called? f)
+    (assert-failure (assert/called? f)
                     "Expected at least 1 call, actual: 0 calls.")
     (f)
     (is (assert/called? f))))
 
 (deftest called-at-least-once-test
   (let [f (spy/stub 42)]
-    (assert-failure #(assert/called-at-least-once? f)
+    (assert-failure (assert/called-at-least-once? f)
                     "Expected at least 1 call, actual: 0 calls.")
     (f)
     (is (assert/called-at-least-once? f))))
@@ -83,7 +82,7 @@
     (f)
     (f)
     (f)
-    (assert-failure #(assert/called-no-more-than-n-times? f 2)
+    (assert-failure (assert/called-no-more-than-n-times? f 2)
                     "Expected no more than 2 calls, actual: 3 calls.")))
 
 (deftest called-no-more-than-once-test
@@ -92,5 +91,5 @@
     (f)
     (is (assert/called-no-more-than-once? f))
     (f)
-    (assert-failure #(assert/called-no-more-than-once? f)
+    (assert-failure (assert/called-no-more-than-once? f)
                     "Expected no more than 1 call, actual: 2 calls.")))
