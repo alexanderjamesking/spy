431dd37fe815790a08f8ee0f45af0dbf1b56fba6
Alex King <alex@alexanderjamesking.com>
2020-11-16 20:40:26 +0100

    Version 2.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tortue/spy "2.1.0"
+(defproject tortue/spy "2.2.0-SNAPSHOT"
   :description "Spy - a Clojure and ClojureScript library for stubs, spies and mocks"
   :url "https://github.com/alexanderjamesking/spy"
   :license {:name "MIT License"
