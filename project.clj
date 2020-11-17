52a8d407bc05a6536256d1e05375d02c58f9e8e7
Rob Hanlon <robhanlon@gmail.com>
2020-11-16 16:12:57 -0800

    Redo CLJS build with shadow-cljs
    This makes some structural changes as well:
    
    * All source files coexist in the same directory
    * All test files coexist in the same directory
    * Target-specific core tests have been merged into the same file using
      reader conditionals
    * Clojure and ClojureScript versions have been bumped
    * Lein version has been bumped in .travis.yml

--- project.clj
+++ project.clj
@@ -5,40 +5,7 @@
             :url "https://opensource.org/licenses/MIT"
             :year 2018
             :key "mit"}
-  :plugins [[lein-doo "0.1.8"]
-            [lein-codox "0.10.3"]
+  :plugins [[lein-codox "0.10.3"]
             [lein-cloverage "1.0.10"]]
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :source-paths ["src/clj" "src/cljc" "src/cljs"]
-  :profiles {:dev {:test-paths ["test/clj" "test/cljc"]
-                   :dependencies [[org.clojure/clojurescript "1.10.238"]]}}
-  :deploy-repositories [["releases" :clojars]]
-  :clean-targets ["target"]
-  :cljsbuild {:builds [{:id "test-phantom"
-                        :source-paths ["src/cljs" "src/cljc" "src/cljs-macros" "test/cljs" "test/cljc/"]
-                        :compiler {:output-to "target/test-phantom/test.js"
-                                   :output-dir "target/test-phantom/out"
-                                   :main 'spy.runner
-                                   :optimizations :none
-                                   :process-shim false}}
-                       {:id "test-nashorn"
-                        :source-paths ["src/cljs" "src/cljc" "src/cljs-macros" "test/cljs" "test/cljc/"]
-                        :compiler {:output-to "target/test-nashorn/test.js"
-                                   :output-dir "target/test-nashorn/out"
-                                   :main 'spy.runner
-                                   :optimizations :simple
-                                   :process-shim false}}
-                       {:id "test-node"
-                        :source-paths ["src/cljs/" "src/cljc/" "src/cljs-macros/" "test/cljs/" "test/cljc/"]
-                        :compiler {:target :nodejs
-                                   :output-to "target/test-node/test.js"
-                                   :output-dir "target/test-node/out"
-                                   :main 'spy.runner
-                                   :optimizations :none
-                                   :process-shim false}}]}
-  :aliases {"test-phantom" ["doo" "phantom" "test-phantom" "once"]
-            "test-nashorn" ["doo" "nashorn" "test-nashorn" "once"]
-            "test-node"    ["doo" "node" "test-node" "once"]
-            "test-cljs"    ["do" ["test-phantom"] ["test-nashorn"] ["test-node"]]
-            "test-quick"   ["do" ["clean"] ["test"] ["test-node"]]
-            "test-all"     ["do" ["clean"] ["test"] ["test-cljs"]]})
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :deploy-repositories [["releases" :clojars]])
