8b40eb70b6c799eb8a4aa2abb3fe07bf89f287b6
Alex King <alex@alexanderjamesking.com>
2021-06-19 17:53:08 +0200

    Re-enable coverage, move clojure and clojurescript to managed deps

--- project.clj
+++ project.clj
@@ -6,8 +6,12 @@
             :year 2018
             :key "mit"}
   :plugins [[lein-codox "0.10.3"]
-            [lein-cloverage "1.1.2"]]
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+            [lein-cloverage "1.2.2"]]
+  :managed-dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojurescript "1.10.866"]]
+  :dependencies [[org.clojure/clojure]
+                 [org.clojure/clojurescript]]
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :test-paths ["test/clj" "test/cljc"]
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories [["releases" :clojars]]
+  :aliases {"coverage" ["cloverage" "--ns-exclude-regex" "spy.test.cljs"]})
